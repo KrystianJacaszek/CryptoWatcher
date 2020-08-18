01e5a7df46b5ac0e156f99556f3f9fc8d0667a7e
Krystian Jacaszek <krystian.jacaszek@gmail.com>
2020-08-18 19:18:23 +0200

    Create base service and interface for reciving data

---
+++ CryptoWatcher/CryptoWatcher/WebServices/WeatherForecastService.cs
@@ -0,0 +1,29 @@
+﻿using CryptoWatcher.Interfaces;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CryptoWatcher.WebServices
+{
+    public class WeatherForecastService: IWeatherForecastService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string wheaterForecastEndpoint = "weatherforecast";
+
+        public WeatherForecastService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecastsAsync()
+        {
+            var response = await _httpClient.GetAsync(wheaterForecastEndpoint);
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = response.Content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(responseBody);
+        }
+    }
+}
