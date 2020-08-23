48963d019d93bbf820c356ae305c2b85e9e979f2
Krystian Jacaszek <krystian.jacaszek@gmail.com>
2020-08-23 15:05:15 +0200

    Create Service and Interface for getting all available coin list

---
+++ CryptoWatcher/API/WebServices/CurrencyInfoService.cs
@@ -0,0 +1,32 @@
+﻿using API.Interfaces;
+using API.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace API.WebServices
+{
+    public class CurrencyInfoService : ICurrencyInfoService
+    {
+        private readonly HttpClient _httpClient;
+        private string mainCurrnecyListUri = "all/coinlist?summary=true";
+
+        public CurrencyInfoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Currency>> GetCurrenciesAsync()
+        {
+            var response = await _httpClient.GetAsync(mainCurrnecyListUri);
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = response.Content.ReadAsStringAsync().Result;
+
+            var deserialziedJson = JsonConvert.DeserializeObject<CoinListRequest>(responseBody);
+
+            return deserialziedJson.Data.Values;
+        }
+    }
+}
