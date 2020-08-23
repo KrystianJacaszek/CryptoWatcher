f3a0832e83874b8c9318bdd369915b8e58c18d1f
Krystian Jacaszek <krystian.jacaszek@gmail.com>
2020-08-23 23:49:57 +0200

    Update controller with new endpoint

--- CryptoWatcher/API/Controllers/CryptoWatcherAPIController.cs
+++ CryptoWatcher/API/Controllers/CryptoWatcherAPIController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Interfaces;
+﻿using API.Interfaces;
 using API.Models;
-using API.WebServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace API.Controllers
 {
@@ -20,10 +18,27 @@ namespace API.Controllers
             _currencyInfoService = currencyInfoService;
         }
 
-        [HttpGet]
-        public async Task<IEnumerable<Currency>> GetCurrenciesList()
+        [HttpGet("currencyList")]
+        public async Task<ActionResult<IEnumerable<Currency>>> GetCurrenciesList()
         {
-            return await _currencyInfoService.GetCurrenciesAsync();
+            var x = await _currencyInfoService.GetCurrenciesAsync();
+            return Ok(JsonConvert.SerializeObject(x));
+        }
+
+        [HttpGet("singleCurrencyInfo")]
+        public async Task<ActionResult<CurrencyBasicInfo>> GetCurrnecyBasicInfo(string symbol)
+        {
+            var x = await _currencyInfoService.GetSingleCurrencyBasicInfoAsync(symbol);
+
+            return Ok(JsonConvert.SerializeObject(x));
+        }
+
+        [HttpPost("multipleCurrencyInfo")]
+        public async Task<ActionResult<IEnumerable<CurrencyBasicInfo>>> GetMultipleCurrnecyBasicInfo(string[] symbolList)
+        {
+            var x = await _currencyInfoService.GetMultipleCurrnecyBasicInfoAsync(symbolList);
+
+            return Ok(JsonConvert.SerializeObject(x));
         }
     }
 }
