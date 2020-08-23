77d454bbf8fb633993f18a47aee95cc0ad2e81c5
Krystian Jacaszek <krystian.jacaszek@gmail.com>
2020-08-23 15:06:50 +0200

    Create controller for getting available coin list

---
+++ CryptoWatcher/API/Controllers/CryptoWatcherAPIController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Interfaces;
+using API.Models;
+using API.WebServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CryptoWatcherAPIController : Controller
+    {
+        private readonly ICurrencyInfoService _currencyInfoService;
+
+        public CryptoWatcherAPIController(ICurrencyInfoService currencyInfoService)
+        {
+            _currencyInfoService = currencyInfoService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Currency>> GetCurrenciesList()
+        {
+            return await _currencyInfoService.GetCurrenciesAsync();
+        }
+    }
+}
