d52f7db9df21095188f983b375dcc67c1ead358f
Krystian Jacaszek <krystian.jacaszek@gmail.com>
2020-08-23 23:50:56 +0200

    Improvements in CurrencyInfoService

--- CryptoWatcher/CryptoWatcher/Interfaces/ICurrencyInfoService.cs
+++ CryptoWatcher/CryptoWatcher/Interfaces/ICurrencyInfoService.cs
@@ -7,5 +7,7 @@ namespace CryptoWatcher.Interfaces
     public interface ICurrencyInfoService
     {
         Task<IEnumerable<Currency>> GetCurrencies();
+        Task<CurrencyBasicInfo> GetSingleCurrencyInfo(string symbol);
+        Task<IEnumerable<CurrencyBasicInfo>> GetMultipleCurrencyInfo(string[] symbolList);
     }
 }
--- CryptoWatcher/CryptoWatcher/WebServices/CurrencyInfoService.cs
+++ CryptoWatcher/CryptoWatcher/WebServices/CurrencyInfoService.cs
@@ -1,8 +1,10 @@
 ﻿using CryptoWatcher.Interfaces;
 using CryptoWatcher.Models;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CryptoWatcher.WebServices
@@ -11,6 +13,9 @@ namespace CryptoWatcher.WebServices
     {
         private readonly HttpClient _httpClient;
         private readonly string currencyApitEndpoint = "CryptoWatcherAPI";
+        private readonly string getCurnnciesList = "/currencyList";
+        private readonly string getCurrencyInfo = "/singleCurrencyInfo";
+        private readonly string getMultipleCurrencyInfo = "/multipleCurrencyInfo";
 
         public CurrencyInfoService(HttpClient httpClient)
         {
@@ -19,12 +24,38 @@ namespace CryptoWatcher.WebServices
 
         public async Task<IEnumerable<Currency>> GetCurrencies()
         {
-            var response = await _httpClient.GetAsync(currencyApitEndpoint);
+            var response = await _httpClient.GetAsync(currencyApitEndpoint + getCurnnciesList);
             response.EnsureSuccessStatusCode();
 
             var responseBody = response.Content.ReadAsStringAsync().Result;
+            string tempDeserialize = (string)JsonConvert.DeserializeObject(responseBody);
 
-            return JsonConvert.DeserializeObject<IEnumerable<Currency>>(responseBody);
+            return JsonConvert.DeserializeObject<IEnumerable<Currency>>(tempDeserialize);
+        }
+
+        public async Task<CurrencyBasicInfo> GetSingleCurrencyInfo(string symbol)
+        {
+            var response = await _httpClient.GetAsync(currencyApitEndpoint + getCurrencyInfo + String.Format("?symbol={0}", symbol));
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = response.Content.ReadAsStringAsync().Result;
+            var tempDeserialize = JsonConvert.DeserializeObject(responseBody);
+
+            return JsonConvert.DeserializeObject<CurrencyBasicInfo>((string)tempDeserialize);
+        }
+
+        public async Task<IEnumerable<CurrencyBasicInfo>> GetMultipleCurrencyInfo(string[] symbolList)
+        {
+            var json = JsonConvert.SerializeObject(symbolList);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(currencyApitEndpoint + getMultipleCurrencyInfo, content);
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = response.Content.ReadAsStringAsync().Result;
+            string tempDeserialize = (string)JsonConvert.DeserializeObject(responseBody);
+
+            return JsonConvert.DeserializeObject<IEnumerable<CurrencyBasicInfo>>(tempDeserialize);
         }
     }
 }
