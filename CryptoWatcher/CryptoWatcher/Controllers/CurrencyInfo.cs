1d3c2a1e924d621279e3240ce2af9022366bae13
Krystian Jacaszek <krystian.jacaszek@gmail.com>
2020-08-23 15:09:14 +0200

    Create contorler CurremcyInfo

---
+++ CryptoWatcher/CryptoWatcher/Controllers/CurrencyInfo.cs
@@ -0,0 +1,32 @@
+﻿using CryptoWatcher.Interfaces;
+using CryptoWatcher.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CryptoWatcher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrencyInfo : ControllerBase
+    {
+        private readonly ILogger<CurrencyInfo> _logger;
+        private readonly ICurrencyInfoService _currencyInfoService;
+
+
+        public CurrencyInfo(ICurrencyInfoService currencyInfoService, ILogger<CurrencyInfo> logger)
+        {
+            _currencyInfoService = currencyInfoService;
+            _logger = logger;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IEnumerable<Currency>> GetAsync()
+        {
+            return await _currencyInfoService.GetCurrencies();
+        }
+    }
+}
